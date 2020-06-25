ad695caeeb7368e27cd89c8c3b855583d2c3a8eb
florianfmmartin <florian.fm.martin@gmail.com>
2020-06-25 17:03:57 -0400

    26.05.20

--- script/zshstart.sh
+++ script/zshstart.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env zsh
 if [ -z "$TMUX" ] && [ -z "$VIRTUAL_ENV" ] && [ -z "$PIPENV_ACTIVE" ]
 then
+    TMUXLS=$(tmux ls 2>&1)
+    if [[ "$TMUXLS" == "no server running on /tmp/tmux-1000/default" ]]
+    then
+        echo -n "tmux sesh: "
+        read INPUT
+    else
         tmux ls
         echo -n "tmux sesh: "
         read INPUT
+    fi
 else
-        INPUT=""
+    INPUT=""
 fi
 
 if [ -z "$INPUT" ]
 then
 else
-        { tmux attach -t $INPUT || tmuxp load $INPUT 2>/dev/null || exec tmux new-session -t $INPUT;}
+    { tmux attach -t $INPUT || tmuxp load $INPUT 2>/dev/null || exec tmux new-session -t $INPUT;}
 fi
 
